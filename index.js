0fa877c0db0f79e261b1b70139a253903651e333
kipernicus <kip.armstrong@gmail.com>
2021-11-08 16:06:25 -0700

    basic post server for testing inter-service integration

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+app.post('/testPost', (req, res) => {
+
+  console.log('DOING THINGS!!!')
+  const cbUrl = req.get('X-Response-URL')
+  setTimeout(() => {
+    axios.post(cbUrl, {
+      data: { notification: 'Thing #1' },
+      notification: 'Thing #2'
+    })
+  }, 5000)
+  res.sendStatus(202)
+})
+app.post('/testPost/:echo', (req, res) => {
+
+  console.log('DOING MORE THINGS!!!', req.params, req.body)
+  const cbUrl = req.get('X-Response-URL')
+  setTimeout(() => {
+    axios.post(cbUrl, {
+      brandNewString: 'Here is your bonus String',
+      echoedString: req.params.echo,
+      reversedString: req.body.reverseMe.split('').reverse().join('')
+    })
+  }, 5000)
+  res.sendStatus(202)
+})
+
+console.log('RUNNING...')
+app.listen(3000)
