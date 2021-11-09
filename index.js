a1bdc1df77012deb94f3631c368e0550f3693205
kipernicus <kip.armstrong@gmail.com>
2021-11-09 16:46:47 -0700

    Better error handling

--- index.js
+++ index.js
@@ -9,26 +9,30 @@ app.post('/testPost', (req, res) => {
 
   console.log('DOING THINGS!!!')
   const cbUrl = req.get('X-Response-URL')
-  setTimeout(() => {
-    axios.post(cbUrl, {
-      data: { notification: 'Thing #1' },
-      notification: 'Thing #2'
-    })
-  }, 5000)
-  res.sendStatus(202)
+  if (cbUrl) {
+    setTimeout(() => {
+      axios.post(cbUrl, {
+        data: { notification: 'Thing #1' },
+        notification: 'Thing #2'
+      })
+    }, 5000)
+  }
+  res.sendStatus(202, 'Boo')
 })
 app.post('/testPost/:echo', (req, res) => {
 
   console.log('DOING MORE THINGS!!!', req.params, req.body)
   const cbUrl = req.get('X-Response-URL')
-  setTimeout(() => {
-    axios.post(cbUrl, {
-      brandNewString: 'Here is your bonus String',
-      echoedString: req.params.echo,
-      reversedString: req.body.reverseMe.split('').reverse().join('')
-    })
-  }, 5000)
-  res.sendStatus(202)
+  if (cbUrl) {
+    setTimeout(() => {
+      axios.post(cbUrl, {
+        brandNewString: 'Here is your bonus String',
+        echoedString: req.params.echo,
+        reversedString: req.body.reverseMe.split('').reverse().join('')
+      })
+    }, 5000)
+  }
+  res.sendStatus(202, 'Yay')
 })
 
 console.log('RUNNING...')
