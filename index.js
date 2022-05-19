33cebf98890be4feaf5f9a0468b065f6ba3cc5ba
kipernicus <kip.armstrong@gmail.com>
2022-05-19 14:11:13 -0600

    More tweaks

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const app = express()
 app.use(express.json())
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+app.get('/ping', (req, res) => {
+  console.log('PING PONG!')
+  return res.status(200).send({ value: 'pong' })
+})
+app.get('/testGet/:echo', (req, res) => {
+  console.log('TESTING GET!')
+  const echo = req.params.echo
+  return res.status(200).send({ echoThis: echo })
+})
 app.post('/testPostOldSchool', (req, res) => {
-
   const cbUrl = req.get('X-Response-URL')
   console.log('OLD SCHOOL POST!!!', JSON.stringify(req.body, null, 2))
-  console.log('URL:', cbUrl)
   if (cbUrl) {
     for (let i = 0; i < 5; i++) {
       setTimeout(() => {
@@ -19,35 +26,122 @@ app.post('/testPostOldSchool', (req, res) => {
       }, 5000)
     }
   }
-  res.sendStatus(202, 'Boo')
+  res.status(202).send('Boo')
 })
-app.post('/testPost', (req, res) => {
-
-  console.log('BASIC POST!!!', JSON.stringify(req.body, null, 2))
+app.post('/testIntegration', (req, res) => {
+  console.log('POST BODY:', JSON.stringify(req.body, null, 2))
+  // console.log('URL PARAMS', JSON.stringify(req.params, null, 2))
+  // console.log('QUERY STRING', JSON.stringify(req.query, null, 2))
   const cbUrl = req.get('X-Response-URL')
-  if (cbUrl) {
-    setTimeout(() => {
-      axios.post(cbUrl, {
-        notification: 'Thing #2'
-      })
-    }, 5000)
+  // if (cbUrl) {
+  //   setTimeout(() => {
+  //     axios.post(cbUrl, {
+  //       notification: 'Thing #2'
+  //     })
+  //   }, 5000)
+  // }
+  res.status(200).send('Boo')
+})
+app.post('/testIntegrationInputs/:input', (req, res) => {
+  console.log('POST BODY:', JSON.stringify(req.body, null, 2))
+  console.log('URL PARAMS', JSON.stringify(req.params, null, 2))
+  console.log('QUERY STRING', JSON.stringify(req.query, null, 2))
+  const cbUrl = req.get('X-Response-URL')
+  // if (cbUrl) {
+  //   setTimeout(() => {
+  //     axios.post(cbUrl, {
+  //       notification: 'Thing #2'
+  //     })
+  //   }, 5000)
+  // }
+  res.status(200).send('Boo')
+})
+app.get('/testIntegrationOutputs', (req, res) => {
+  try {
+    const responseData = {
+      text: 'text',
+      email: 'mail@test.com',
+      number: 17,
+      bool: true,
+      department: { id: '618b0bbd6f65647cddf786cd', label: 'Science' },
+      user: { id: '618b0b6a6f6564f5bef78665', label: 'Joe Stone' },
+      usersList: [
+        { id: '618b0b6a6f6564f5bef78665', label: 'Joe Stone' },
+        { id: '618b0b386f656410c9f78653', label: 'Ray Call' }
+      ],
+      richText: '<p>Here is some fancy text</p>'
+    }
+    res.status(200).json(responseData)
+  } catch (err) {
+    console.error('DEATH', err)
+    res.status(500).send(err.message)
   }
-  res.sendStatus(202, 'Boo')
+})
+app.post('/kd-demo', (req, res) => {
+  try {
+    const {
+      attendees,
+      distance,
+      costPerMile
+    } = req.body
+    const result = attendees * distance * costPerMile
+    const supervisor = { id: '618b0b6a6f6564f5bef78665', label: 'Joe Stone' }
+    const responseData = {
+      result,
+      supervisor
+    }
+    res.status(200).json(responseData)
+  } catch (err) {
+    console.error('DEATH', err)
+    res.status(500).send(err.message)
+  }
 })
 app.post('/testPost/:echo', (req, res) => {
-
-  console.log('ADVANCED POST!!!', req.params, req.body)
-  const cbUrl = req.get('X-Response-URL')
-  if (cbUrl) {
-    setTimeout(() => {
-      axios.post(cbUrl, {
-        brandNewString: 'Here is your bonus String',
-        echoedString: req.params.echo,
-        reversedString: req.body.reverseMe.split('').reverse().join('')
-      })
-    }, 5000)
+  try {
+    const cbUrl = req.get('X-Response-URL')
+    const echo = req.params.echo || ''
+    console.log('BODY IS', req.body)
+    const responseData = {
+      brandNewString: 'Here is your bonus String (edited)',
+      echoedString: req.params.echo,
+      reversedString: req.body.reverseMe
+        ? req.body.reverseMe
+          .split('')
+          .reverse()
+          .join('')
+        : 'nothin',
+      calc: '500',
+      text: 'text',
+      email: 'mail@test.com',
+      number: 17,
+      bool: true,
+      department: { id: '618b0bbd6f65647cddf786cd', label: 'Science' },
+      user: { id: '618b0b6a6f6564f5bef78665', label: 'Joe Stone' },
+      usersList: [
+        { id: '618b0b6a6f6564f5bef78665', label: 'Joe Stone' },
+        { id: '618b0b386f656410c9f78653', label: 'Ray Call' }
+      ],
+      richText: '<p>Here is some fancy text</p>',
+      garbage: 'breakage'
+    }
+    console.log('ECHO', echo)
+    if (echo.indexOf('timeout') !== -1) {
+      console.log('TIMING OUT!!!')
+      setTimeout(() => {
+        res.status(200).json(responseData)
+      }, 10000)
+    } else if (cbUrl) {
+      setTimeout(() => {
+        axios.post(cbUrl, responseData)
+      }, 5000)
+      res.status(202).send('Yay')
+    } else {
+      res.status(200).send(responseData)
+    }
+  } catch (err) {
+    console.error('DEATH', err)
+    res.status(500).send(err.message)
   }
-  res.sendStatus(202, 'Yay')
 })
 
 console.log('RUNNING...')
